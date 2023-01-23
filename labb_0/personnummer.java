5a649fd01b8a10dc769407745ccedb6c101c38c5
edvardakerman <ed@sagovagen.se>
2023-01-23 17:34:02 +0100

    git ignore

--- labb_0/personnummer.java
+++ labb_0/personnummer.java
@@ -13,7 +13,7 @@ class personnummer {
 
     }
 
-    // Metod för att hämta data från consolen, (personnummer)
+    // Metod för att hämta data från consolen, (personnummer) test
     public static String getString() {
 
 		Scanner input = new Scanner(System.in);
